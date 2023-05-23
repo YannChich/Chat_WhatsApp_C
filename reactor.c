7c0b3bd0655930d417a796103855f6c9c1d31d44
Yann Chicheportiche <118822003+YannChich@users.noreply.github.com>
2023-05-23 15:52:04 +0300

    Update reactor.c

--- reactor.c
+++ reactor.c
@@ -5,7 +5,9 @@
 
 reactor_t* createReactor() {
     reactor_t* reactor = malloc(sizeof(reactor_t));
-    reactor->handlers = malloc(sizeof(handler_t) * MAX_FD); // Allocate memory for handlers
+    reactor->fd_capacity = 10;  // Initial capacity
+    reactor->fds = malloc(sizeof(struct pollfd) * reactor->fd_capacity);
+    reactor->handlers = malloc(sizeof(handler_t) * reactor->fd_capacity);
     reactor->fd_count = 0;
     reactor->running = 0;
     return reactor;
@@ -40,10 +42,30 @@ void startReactor(reactor_t* reactor) {
 }
 
 void addFd(reactor_t* reactor, int fd, handler_t handler) {
+    // Check if the arrays need to be resized
+    if (reactor->fd_count >= reactor->fd_capacity) {
+        reactor->fd_capacity *= 2;  // Double the capacity
+        reactor->fds = realloc(reactor->fds, sizeof(struct pollfd) * reactor->fd_capacity);
+        reactor->handlers = realloc(reactor->handlers, sizeof(handler_t) * reactor->fd_capacity);
+    }
+
     reactor->fds[reactor->fd_count].fd = fd;
     reactor->fds[reactor->fd_count].events = POLLIN;
     reactor->handlers[reactor->fd_count] = handler;
     reactor->fd_count++;
+}
+
+void removeFd(reactor_t* reactor, int fd) {
+    // Find the fd in the array
+    for (int i = 0; i < reactor->fd_count; i++) {
+        if (reactor->fds[i].fd == fd) {
+            // Move the other fds down
+            memmove(&reactor->fds[i], &reactor->fds[i+1], (reactor->fd_count - i - 1) * sizeof(struct pollfd));
+            memmove(&reactor->handlers[i], &reactor->handlers[i+1], (reactor->fd_count - i - 1) * sizeof(handler_t));
+            reactor->fd_count--;
+            break;
+        }
+    }
 }
 
 void waitFor(reactor_t* reactor) {
